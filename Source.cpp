9dda3ee2f4ae3c7d67dbe0e7069580fcdaa86b0a
hackerd25 <100475460+hackerd25@users.noreply.github.com>
2022-06-14 19:20:12 +0300

    Ractangle
    It is a helpful tool for ractangles.
    If it has bugs it's my first program so text me.

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    unsigned short A = 10;
+    unsigned short B = 69;
+    unsigned short P = A + B + A + B;
+    unsigned short S = A * B;
+
+    std::cout << "Perimeter of the ractangle is " << P << "\n";
+    std::cout << "The area of the ractangle is " << S << "\n";
+return 0
+}
